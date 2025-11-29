e226478af693baa5a53c003636d37b61db28a361
Erisuuuuu <79221766+Erisuuuuu@users.noreply.github.com>
2025-11-29 12:56:31 +0300

    всё собрал вместе пазл с картой и карточками

---
+++ Stasis-ChifuPointNClickBranch/Assets/Scripts/PieceDrag.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PieceDrag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    [Header("Настройки")]
+    public RectTransform target; // сюда привязываем правильную позицию
+    public float snapDistance = 50f; // расстояние магнита
+
+    private RectTransform rect;
+    private Canvas canvas;
+
+    void Awake()
+    {
+        rect = GetComponent<RectTransform>();
+        canvas = GetComponentInParent<Canvas>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        // Чтобы быть поверх других
+        transform.SetAsLastSibling();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        // Перемещение вместе с курсором
+        rect.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        float dist = Vector2.Distance(rect.anchoredPosition, target.anchoredPosition);
+
+        if (dist < snapDistance)
+        {
+            // Автоприцепление
+            rect.anchoredPosition = target.anchoredPosition;
+            enabled = false; // запретить дальнейшее перетягивание
+        }
+    }
+}
